0f378aa454c340acb419a48db6cb89bde53fc62b
jieunb3333 <jieun1172@naver.com>
2020-06-08 00:00:10 +0900

    w

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -4,4 +4,4 @@ from .models import Portfolio
 class CreatePortfolioForm(forms.ModelForm):
     class Meta:
         model = Portfolio
-        fields = ['image','pakage_num','pakage_name','max_person','pre_person','during_date','description']
+        fields = ['image','description']
